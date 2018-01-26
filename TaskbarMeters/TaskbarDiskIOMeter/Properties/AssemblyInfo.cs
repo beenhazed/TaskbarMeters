d506f5af6f93cabddc0a524ca86c68ec8035c5d8
Ben Hayes <Ben.Hayes@zaner-bloser.com>
2018-01-26 10:42:47 -0500

    Moved to .NET 4.6.1, moved references of the Windows API Code Pack to use NuGet project by aybe (https://www.nuget.org/profiles/aybe), and added a Visual Studio Installer project for making an installation package.

--- TaskbarMeters/TaskbarDiskIOMeter/Properties/AssemblyInfo.cs
+++ TaskbarMeters/TaskbarDiskIOMeter/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -17,7 +16,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("78d90e0d-fb6a-49ed-a1c6-934966fcf3cb")]
