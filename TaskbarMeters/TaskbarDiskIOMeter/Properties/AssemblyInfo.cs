a5c2cde7dc329bce10b4b2a0c96c9da5fa7afd11
Benjamin Hayes <beenhazed@gmail.com>
2018-01-30 14:40:26 -0500

    Updated to get working installation version

--- TaskbarMeters/TaskbarDiskIOMeter/Properties/AssemblyInfo.cs
+++ TaskbarMeters/TaskbarDiskIOMeter/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("78d90e0d-fb6a-49ed-a1c6-934966fcf3cb")]
